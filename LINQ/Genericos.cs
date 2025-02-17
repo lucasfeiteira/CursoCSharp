8bcbd94821ae4706fab7741bbcfa875d47507c9b
Lucas Henrique dos Santos Feiteira <lucas.feiteira@outlook.com>
2025-02-17 14:54:39 -0300

    .

---
+++ LINQ/Genericos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.LINQ {
+    internal class Genericos {
+
+        public static void Executar() {
+
+        }
+    }
+}
